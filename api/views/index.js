380eabf9f1b8bc94de895f9b922990ae2092fe0f
Harshit Agarwal <Harshit@harshit.local>
2017-05-11 00:14:11 -0700

    committed by harshit

--- api/views/index.js
+++ api/views/index.js
@@ -1,14 +1,14 @@
- function submitlogin() {
-        //alert("hihi")
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-      //alert("bibi");
-      //alert(this.readyState);
-      //alert(this.status);
-    if (this.readyState == 4 && this.status == 200) {
-     console.log(this.responseText);
-    }
-  };
-  xhttp.open("GET", "http://localhost:3000/login?userid="+document.getElementById('username').value + "&password="+ document.getElementById('password').value, true);
-  xhttp.send();
-}
+// function submitlogin() {
+//        //alert("hihi")
+//  var xhttp = new XMLHttpRequest();
+//  xhttp.onreadystatechange = function() {
+//      //alert("bibi");
+//      //alert(this.readyState);
+//      alert(this.status);
+//    if (this.readyState == 4 && this.status == 200) {
+//     console.log(this.responseText);
+//    }
+//  };
+//  xhttp.open("GET", "http://localhost:3000/login?userid="+document.getElementById('username').value + "&password="+ document.getElementById('password').value, true);
+//  xhttp.send();
+//}
